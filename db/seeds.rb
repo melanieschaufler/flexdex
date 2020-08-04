f39f4cfb0f0939be42c10a0d73b0dfa7a6771505
Melanie Schaufler <melanie.schaufler@yahoo.de>
2020-08-04 16:00:22 +0200

    Seedfile updatet and fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+User.destroy_all
+puts "Creating user"
+melanie = {email: "melanie@web.de", password: "123456"}
+carla = {email: "carla@web.de", password: "123456"}
+louisa = {email: "louisa@web.de", password: "123456"}
+vincent = {email: "vincent@web.de", password: "123456"}
+[ melanie, vincent, carla, louisa ].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.email}"
+end
+puts "Finished!"
+puts "Cleaning database..."
+Workspace.destroy_all
+puts "Creating workspaces"
+munich_central = {user: User.find_by_email("melanie@web.de"), address: "Hohenzollernstraße 18", description:"Cozy workspace with stable Wifi and openminded people", price: 15, seats: 20, name:"Foodora open space"}
+munich_east = {user: User.find_by_email("carla@web.de"), address: "Regerstraße 100", description:"Open workspace close to munich east station", price: 23, seats: 25, name:"BCG workspace"}
+frankfurt_central = {user: User.find_by_email("carla@web.de"), address: "Gallusstraße 20", description:"Comfortable workspace close to FFM central station", price: 7, seats: 40, name:"Deutsche Bank Finance workspace"}
+[ munich_central, munich_east, frankfurt_central ].each do |attributes|
+  workspace = Workspace.create!(attributes)
+  puts "Created #{workspace.name}"
+end
+puts "Finished!"

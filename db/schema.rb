db4de01e35b81968446a987d37415a9a9aa65b44
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-08-06 17:30:08 +0200

    Bookings accepted with complete routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_091214) do
+ActiveRecord::Schema.define(version: 2020_08_06_135018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_091214) do
     t.bigint "workspace_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "false"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["workspace_id"], name: "index_bookings_on_workspace_id"
   end

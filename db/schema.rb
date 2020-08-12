0030df43c618281f64917b09a463f931ececc8e3
Vincent Michler vincent.michler94@gmail.com <vincent.michler94@gmail.com>
2020-08-12 10:16:51 +0200

    To be deleted

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_145057) do
     t.bigint "workspace_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "status", default: "pending"
+    t.string "status", default: "false"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["workspace_id"], name: "index_bookings_on_workspace_id"
   end
